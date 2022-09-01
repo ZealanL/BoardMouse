fcf7d1cb9fb799a93799f13d68b9a8a90bc4edea
ZealanL <zlaferte@gmail.com>
2022-08-31 17:24:45 -0700

    Add constructors for Pos struct

--- src/Engine/Pos/Pos.h
+++ src/Engine/Pos/Pos.h
@@ -5,6 +5,11 @@
 struct Pos {
 	byte index; // Positional index
 
+	Pos(byte index = 0) : index(index) {}
+	Pos(int x, int y) {
+		index = x + (y * BD_SIZE);
+	}
+
 	// Returns false if index is out-of-range
 	bool IsValid();
 };
