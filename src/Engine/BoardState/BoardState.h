a90c105468400c6ca7fd9afe62e3464d04d75238
ZealanL <zlaferte@gmail.com>
2022-09-04 21:58:01 -0700

    Modify BoardState struct properties

--- src/Engine/BoardState/BoardState.h
+++ src/Engine/BoardState/BoardState.h
@@ -6,12 +6,21 @@
 struct BoardState {
 	struct {
 
-		BitBoard piecePositions[PT_AMOUNT];
+		// Has a single bit for the position of the king
+		BitBoard kingPos;
+
+		// What squares we have pieces in
 		BitBoard occupy;
+
+		// What squares we can attack
+		BitBoard attack;
 
 		bool canCastleL, canCastleR;
 	} teamData[TEAM_AMOUNT];
 	
+	// What pieces are at what positions
+	uint8 pieceTypes[BD_SQUARE_AMOUNT];
+
 	// Normally blank, has a single bit on when en passant is possible
 	BitBoard enPassantMask;
 };
