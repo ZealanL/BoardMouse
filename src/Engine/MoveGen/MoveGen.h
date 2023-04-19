b931b3f9f0cce9485ccf507e1abd4dde04ea5070
ZealanL <zlaferte@gmail.com>
2023-04-19 14:00:44 -0700

    Remove unused macro MAX_MOVES_PER_TURN

--- src/Engine/MoveGen/MoveGen.h
+++ src/Engine/MoveGen/MoveGen.h
@@ -1,9 +1,6 @@
 #pragma once
 #include "../BoardState/BoardState.h"
 
-// TODO: There can maybe be more than this?
-#define MAX_MOVES_PER_TURN 600
-
 namespace MoveGen {
 	void GetMoves(BoardState& board, vector<BoardState::Move>& movesOut);
 }
