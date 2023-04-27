052f186506e7c6d921f2dfed81fdd3705da58be3
ZealanL <zlaferte@gmail.com>
2023-04-27 15:16:00 -0700

    Add Engine namespace with search functions

---
+++ src/Engine/Engine.cpp
@@ -0,0 +1,289 @@
+#include "Engine.h"
+
+#include "Transpos/Transpos.h"
+#include "MoveGen/MoveGen.h"
+
+BoardState::Move g_CurPV[MAX_SEARCH_DEPTH] = {};
+uint16_t g_CurPVLength = 0;
+
+uint8_t g_CurState = Engine::STATE_INITIALIZING;
+BoardState g_Position = BoardState();
+Engine::Stats g_Stats = Engine::Stats();
+std::mutex infoMutex = std::mutex();
+
+// NOTE: infoMutex does not apply to this, as this variable needs to be checked very frequently
+bool g_StopSearch = false;
+
+uint8_t Engine::GetState() {
+	infoMutex.lock();
+	uint8_t result = g_CurState;
+	infoMutex.unlock();
+	return result;
+}
+
+void Engine::SetState(uint8_t state) {
+	infoMutex.lock();
+	g_CurState = state;
+	infoMutex.unlock();
+}
+
+BoardState Engine::GetPosition() {
+	infoMutex.lock();
+	BoardState result = g_Position;
+	infoMutex.unlock();
+	return result;
+}
+
+vector<BoardState::Move> Engine::GetCurrentPV() {
+	infoMutex.lock();
+	if (g_CurPVLength == 0) {
+		infoMutex.unlock();
+		return {};
+	} else {
+		ASSERT(g_CurPVLength <= MAX_SEARCH_DEPTH);
+		auto result = vector<BoardState::Move>(g_CurPV, g_CurPV + g_CurPVLength);
+		infoMutex.unlock();
+		return result;
+	}
+}
+
+Engine::Stats Engine::GetStats() {
+	infoMutex.lock();
+	Stats result = g_Stats;
+	infoMutex.unlock();
+	return result;
+}
+
+void Engine::SetPosition(const BoardState& boardState) {
+	infoMutex.lock();
+	// NOTE: It's perfectly fine if we are in a search when this happens.
+	//	That search will continue with the previous state.
+	g_Position = boardState;
+	infoMutex.unlock();
+}
+
+void Engine::StopSearch() {
+	infoMutex.lock();
+	if (g_CurState == STATE_SEARCHING) {
+		g_StopSearch = true;
+	}
+	infoMutex.unlock();
+}
+
+enum class MinMaxResult {
+	NONE,
+	NEW_BEST,
+	PRUNE_BRANCH
+};
+
+template <uint8_t TEAM>
+FINLINE MinMaxResult UpdateMinMax(Value eval, Value& min, Value& max) {
+	if ((TEAM == TEAM_WHITE) ? (eval >= max) : (eval <= min)) {
+		// Branch will not be chosen because it already has a move that is worse than any move in another branch
+		// We can just skip this whole branch
+		return MinMaxResult::PRUNE_BRANCH;
+	}
+
+	if ((TEAM == TEAM_WHITE) ? (eval > min) : (eval < max)) {
+		(TEAM == TEAM_WHITE ? min : max) = eval;
+		return MinMaxResult::NEW_BEST;
+	} else {
+		return MinMaxResult::NONE;
+	}
+}
+
+// How much to reserve in the move vector
+#define MOVE_RESERVE_AMOUNT 60
+
+template <uint8_t TEAM>
+Value MinMaxSearchRecursive(BoardState& boardState, Value min, Value max, uint16_t depth, uint16_t pvIndex) {
+
+	if (g_StopSearch)
+		return (TEAM == TEAM_WHITE) ? min : max;
+
+	ASSERT(boardState.turnTeam == TEAM);
+
+	TransposEntry* entry = Transpos::Find(boardState.hash);
+	bool entryHashMatches = (entry->fullHash == boardState.hash);
+	if (entryHashMatches)
+		g_Stats.transposHits++;
+
+	if (entryHashMatches && entry->depth >= depth) {
+		// We've already evaluated this move at >= the current search depth, just use that
+		g_Stats.transposOverrides++;
+
+		Value score = entry->eval;
+		MinMaxResult minMaxResult = UpdateMinMax<TEAM>(entry->eval, min, max);
+		if (minMaxResult == MinMaxResult::PRUNE_BRANCH) {
+			return (TEAM == TEAM_WHITE) ? max : min;
+		}
+	} else {
+		// No transposition entry for this position is as deep
+
+		if (depth == 0) {
+			g_Stats.leafNodesEvaluated++;
+			Value 
+				whiteVal = boardState.teamData[TEAM_WHITE].totalValue,
+				blackVal = boardState.teamData[TEAM_BLACK].totalValue;
+			return (whiteVal - blackVal);
+
+			// NOTE: We won't bother setting a transposition entry for a zero-depth evaluation
+		} else {
+			// TODO: Probably should have a list of pre-allocated move vectors, or maybe not store moves at all
+			vector<BoardState::Move> moves;
+			moves.reserve(MOVE_RESERVE_AMOUNT);
+			MoveGen::GetMoves(boardState, moves);
+
+			size_t moveCount = moves.size();
+
+			if (moveCount == 0) {
+				if (boardState.teamData[!TEAM].checkers != 0) {
+					// Checkmate!
+					// Other team wins
+					g_Stats.matesFound++;
+					return (TEAM == TEAM_WHITE) ? -CHECKMATE_VALUE : CHECKMATE_VALUE;
+				} else {
+					// Stalemate
+					g_Stats.stalematesFound++;
+					return 0;
+				}
+			} else {
+				size_t lastBestMoveIndex;
+				if (entryHashMatches) {
+					lastBestMoveIndex = entry->bestMoveIndex;
+
+					if (lastBestMoveIndex < moveCount) {
+						// Explore the best move first
+						moves.push_back(moves[lastBestMoveIndex]);
+						moveCount++;
+					} else {
+						// Hash collision!
+						// This entry no longer matches
+						g_Stats.transposBadMoveIndices++;
+						lastBestMoveIndex = -1;
+						entryHashMatches = false;
+					}
+				} else {
+					lastBestMoveIndex = -1;
+				}
+
+				size_t bestMoveIndex = 0;
+				for (size_t i = moveCount - 1; i < moveCount; i--) {
+					if (lastBestMoveIndex == i) {
+						// We already added this move to the end of the vector
+						// Ignore its original location
+						continue;
+					}
+
+					BoardState::Move& move = moves[i];
+
+					BoardState boardCopy = boardState;
+					boardCopy.ExecuteMove(move);
+					Value eval = MinMaxSearchRecursive<!TEAM>(boardCopy, min, max, depth - 1, pvIndex + 1);
+
+					MinMaxResult minMaxResult = UpdateMinMax<TEAM>(eval, min, max);
+					if (minMaxResult == MinMaxResult::NEW_BEST) {
+						bestMoveIndex = i;
+					} else if (minMaxResult == MinMaxResult::PRUNE_BRANCH) {
+						return (TEAM == TEAM_WHITE) ? max : min;
+					}
+				}
+
+				// Update table entry
+				entry->bestMoveIndex = bestMoveIndex;
+				entry->fullHash = boardState.hash;
+				entry->depth = depth;
+				entry->eval = (TEAM == TEAM_WHITE) ? min : max;
+
+				g_CurPV[pvIndex] = moves[bestMoveIndex];
+			}
+		}
+	}
+
+	return (TEAM == TEAM_WHITE) ? min : max;
+}
+
+uint8_t Engine::DoSearch(uint16_t depth, size_t maxTimeMS) {
+	ASSERT(depth > 0);
+
+#ifdef _DEBUG
+	ASSERT(depth <= MAX_SEARCH_DEPTH);
+#else
+	depth = MIN(depth, MAX_SEARCH_DEPTH);
+#endif
+
+	size_t startTimeMS = CUR_MS();
+	uint8_t previousState = STATE_READY;
+
+	BoardState initialBoardState = GetPosition();
+	infoMutex.lock();
+	{
+		if (g_CurState != STATE_READY) {
+			// We aren't able to search right now
+			infoMutex.unlock();
+			return SEARCH_COULDNT_START;
+		} else {
+			previousState = g_CurState;
+			g_CurState = STATE_SEARCHING;
+		}
+
+		g_CurPVLength = 0;
+	}
+	infoMutex.unlock();
+
+	vector<BoardState::Move> initialMoves;
+	MoveGen::GetMoves(initialBoardState, initialMoves);
+	bool isWhite = (initialBoardState.turnTeam == TEAM_WHITE);
+
+	if (!initialMoves.empty()) {
+
+		// Iterative deepening search
+		for (uint16_t curDepth = 1; (curDepth <= depth) && (!g_StopSearch); curDepth++) {
+			size_t msElapsed = CUR_MS() - startTimeMS;
+			if (msElapsed > maxTimeMS && maxTimeMS > 0)
+				break;
+
+			DLOG("Searching at depth " << curDepth << "/" << depth << "...");
+
+			infoMutex.lock();
+			g_CurPVLength = curDepth;
+			g_Stats = Stats();
+			infoMutex.unlock();
+
+			Value bestEval;
+			if (isWhite) {
+				bestEval = MinMaxSearchRecursive<TEAM_WHITE>(initialBoardState, -CHECKMATE_VALUE, CHECKMATE_VALUE, curDepth, 0);
+			} else {
+				bestEval = MinMaxSearchRecursive<TEAM_BLACK>(initialBoardState, -CHECKMATE_VALUE, CHECKMATE_VALUE, curDepth, 0);
+			}
+
+			{ // Print UCI info
+				// TODO: Move this all to UCI.cpp, use a callback function to trigger print
+				std::stringstream uciInfo;
+				uciInfo << "info";
+				uciInfo << " depth " << curDepth;
+				uciInfo << " multipv 1"; // TODO: Support multi-PV
+				uciInfo << " score cp " << (bestEval * (isWhite ? 1 : -1));
+				uciInfo << " nodes " << g_Stats.leafNodesEvaluated;
+
+				size_t nps = (size_t)(g_Stats.leafNodesEvaluated / (MAX(msElapsed, 500) / 1000.f));
+				uciInfo << " nps " << nps;
+				uciInfo << " time " << msElapsed;
+
+				uciInfo << " pv ";
+				for (int i = 0; i < g_CurPVLength; i++) {
+					uciInfo << g_CurPV[i] << ' ';
+				}
+				LOG(uciInfo.str());
+			}
+		}
+	}
+
+	infoMutex.lock();
+	{
+		g_CurState = previousState;
+	}
+	infoMutex.unlock();
+
+	return SEARCH_COMPLETED;
+}
---
+++ src/Engine/Engine.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "BoardState/BoardState.h"
+#include "LookupGen/LookupGen.h"
+
+#define MAX_SEARCH_DEPTH 4096
+
+namespace Engine {
+
+	struct Stats {
+		// Evaluation (absolute, positive = white is winning, negative = black is winning)
+		Value eval;
+
+		// The last depth of the completed iteration
+		uint16_t completedDepth;
+
+		// Number of positions reached at depth 0
+		uint64_t leafNodesEvaluated; 
+
+		// Number of times we found a position within the transposition table
+		uint64_t transposHits;
+
+		// Number of times we used the previous result from the transposition table 
+		//	instead of continuing our current search
+		uint64_t transposOverrides;
+
+		// Number of times the transposition table returned a position with a matching hash,
+		//	but an invalid move index (hash collision)
+		uint64_t transposBadMoveIndices;
+
+		// Number of checkmates found in the current search (for either color)
+		uint64_t matesFound;
+
+		// Number of stalemates found in the current search (for either color)
+		uint64_t stalematesFound;
+
+		Stats() = default;
+	};
+
+	enum {
+		STATE_INITIALIZING,
+		STATE_READY,
+		STATE_SEARCHING
+	};
+
+	uint8_t GetState();
+	void SetState(uint8_t state);
+	BoardState GetPosition();
+	vector<BoardState::Move> GetCurrentPV();
+
+	Stats GetStats(); // NOTE: Stats are reset every new search
+
+	void SetPosition(const BoardState& state);
+
+	enum {
+		SEARCH_COULDNT_START,
+		SEARCH_INTERRUPTED,
+		SEARCH_COMPLETED
+	};
+
+	uint8_t DoSearch(uint16_t depth, size_t maxTimeMS = -1);
+	void StopSearch();
+}
