aa1d806524ca1e9c5f43bc5cfede8384a4902c0a
ZealanL <zlaferte@gmail.com>
2023-04-27 15:15:31 -0700

    Add FEN::STARTPOS

--- src/FEN/FEN.h
+++ src/FEN/FEN.h
@@ -2,6 +2,8 @@
 #include "../Engine/BoardState/BoardState.h"
 
 namespace FEN {
+	constexpr const char* STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
 	void Parse(vector<string> parts, BoardState& boardStateOut);
 	inline void Parse(string parts, BoardState& boardStateOut) {
 		Parse(SPLIT_STR(parts, " "), boardStateOut);
