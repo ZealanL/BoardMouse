8e19eb1a2696e4fc228c19c4ce503af8f6da911f
ZealanL <zlaferte@gmail.com>
2023-05-01 16:50:55 -0700

    Relocate MoveList from MoveGen.h to Move.h

--- src/Engine/Move/Move.h
+++ src/Engine/Move/Move.h
@@ -20,3 +20,31 @@ struct Move {
 
 	friend std::ostream& operator<<(std::ostream& stream, const Move& move);
 };
+
+#define MAX_MOVES 320
+struct MoveList {
+	Move data[MAX_MOVES];
+	size_t size = 0;
+
+	FINLINE void Add(const Move& move) {
+		data[size] = move;
+		size++;
+		ASSERT(size <= MAX_MOVES);
+	}
+
+	FINLINE Move& operator[](size_t index) {
+		ASSERT((index < size) && (index < MAX_MOVES));
+		return data[index];
+	}
+
+	FINLINE const Move& operator[](size_t index) const {
+		ASSERT((index < size) && (index < MAX_MOVES));
+		return data[index];
+	}
+
+	FINLINE const Move* begin() const { return data; }
+	FINLINE Move* begin() { return data; }
+
+	FINLINE const Move* end() const { return data + size; }
+	FINLINE Move* end() { return data + size; }
+};
--- src/Engine/MoveGen/MoveGen.h
+++ src/Engine/MoveGen/MoveGen.h
@@ -3,34 +3,6 @@
 
 typedef std::function<void(const Move& move)> MoveCallbackFn;
 
-#define MAX_MOVES 320
-struct MoveList {
-	Move data[MAX_MOVES];
-	size_t size = 0;
-
-	FINLINE void Add(const Move& move) {
-		data[size] = move;
-		size++;
-		ASSERT(size <= MAX_MOVES);
-	}
-
-	FINLINE Move& operator[](size_t index) {
-		ASSERT((index < size) && (index < MAX_MOVES));
-		return data[index];
-	}
-
-	FINLINE const Move& operator[](size_t index) const {
-		ASSERT((index < size) && (index < MAX_MOVES));
-		return data[index];
-	}
-
-	FINLINE const Move* begin() const { return data; }
-	FINLINE Move* begin() { return data; }
-
-	FINLINE const Move* end() const { return data + size; }
-	FINLINE Move* end() { return data + size; }
-};
-
 namespace MoveGen {
 	void GetMoves(const BoardState& board, MoveList& movesOut);
 	void GetMoves(const BoardState& board, MoveCallbackFn callbackFn);
