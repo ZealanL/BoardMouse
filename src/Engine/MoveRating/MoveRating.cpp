976c51bc61d7147d95820d61815240944f8b2851
ZealanL <zlaferte@gmail.com>
2023-05-10 15:26:59 -0700

    Add move rating bonus for promotion, slightly reorginize MoveRating::RateMoves()

--- src/Engine/MoveRating/MoveRating.cpp
+++ src/Engine/MoveRating/MoveRating.cpp
@@ -3,6 +3,11 @@
 #include "../LookupGen/LookupGen.h"
 
 void MoveRating::RateMoves(BoardState& boardState, MoveList& moves) {
+
+	constexpr Value
+		CAPTURE_BASE_BONUS = 100,
+		PROMOTION_QUEEN_BASE_BONUS = 1000;
+
 	uint8_t team = boardState.turnTeam;
 	auto& td = boardState.teamData[team];
 	auto& etd = boardState.teamData[!team];
@@ -12,10 +17,14 @@ void MoveRating::RateMoves(BoardState& boardState, MoveList& moves) {
 	for (Move& move : moves) {
 		Value rating = 0;
 
-		// Captures always have a decent bonus
-		if (move.flags | Move::FL_CAPTURE)
-			rating += 100;
-			
+		{ // Add base bonuses
+			if (move.flags | Move::FL_CAPTURE)
+				rating += CAPTURE_BASE_BONUS;
+
+			if ((move.flags | Move::FL_PROMOTION) && (move.resultPiece == PT_QUEEN))
+				rating += PROMOTION_QUEEN_BASE_BONUS;
+		}
+
 		// If normal capture, add change in value to rating
 		if (etd.occupy[move.to]) {
 			rating += boardState.pieceValues[move.to];
